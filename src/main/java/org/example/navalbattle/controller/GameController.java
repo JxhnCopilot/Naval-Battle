03087468a211e13b73c2bc427cf4f6f1f361339d
johnramosg <ramos.john@correounivalle.edu.co>
2024-05-30 20:06:02 -0500

    Modifique el archivo fxml game-view

--- src/main/java/org/example/navalbattle/controller/GameController.java
+++ src/main/java/org/example/navalbattle/controller/GameController.java
@@ -1,4 +1,19 @@
 package org.example.navalbattle.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+
+import java.io.IOException;
+
 public class GameController {
+    @FXML
+    private GridPane positionGridPane;
+    @FXML
+    private GridPane shootGridpane;
+    public void onHandleButtonPlay(ActionEvent event) throws IOException {
+        positionGridPane.setVisible(true);
+        shootGridpane.setVisible(true);
+    }
 }
