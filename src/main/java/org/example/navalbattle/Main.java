70c5b9f9ac1cbe4beaa721a105590a7816d87be7
johnramosg <ramos.john@correounivalle.edu.co>
2024-05-30 21:24:07 -0500

    Agregue evento de click al tablero de disparo

--- src/main/java/org/example/navalbattle/Main.java
+++ src/main/java/org/example/navalbattle/Main.java
@@ -1,19 +1,24 @@
 package org.example.navalbattle;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import org.example.navalbattle.view.WelcomeStage;
 
 import java.io.IOException;
 
-public class Main extends Application {
-
+public class Main extends Application { ;
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
         WelcomeStage.getInstance();
     }
 }
+
--- src/main/java/org/example/navalbattle/controller/GameController.java
+++ src/main/java/org/example/navalbattle/controller/GameController.java
@@ -2,7 +2,10 @@ package org.example.navalbattle.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 
 import java.io.IOException;
@@ -12,8 +15,40 @@ public class GameController {
     private GridPane positionGridPane;
     @FXML
     private GridPane shootGridpane;
+    @FXML
+    private GridPane positionGridPane1;
+    @FXML
+    private Button startButton;
+    @FXML
+    public void initialize() {
+        addEvent();
+    }
     public void onHandleButtonPlay(ActionEvent event) throws IOException {
         positionGridPane.setVisible(true);
         shootGridpane.setVisible(true);
+        positionGridPane1.setVisible(false);
+        startButton.setVisible(false);
+    }
+    private void handleMouseClick(MouseEvent event, int row, int col) {
+        System.out.println("Clicked on cell: (" + row + ", " + col + ")");
+        // Aquí puedes agregar la lógica adicional que necesitas al hacer clic en una celda.
+    }
+    public void addEvent() {
+        for (int row = 0; row <= 9; row++) {
+            for (int col = 0; col <= 9; col++) {
+                Pane pane = new Pane();
+                final int r = row;
+                final int c = col;
+                pane.setOnMouseClicked(event -> handleMouseClick(event, r, c));
+                pane.setStyle("-fx-background-color: blue;" +
+                                "-fx-border-color: black;"+
+                        "-fx-border-width: 2px");
+                shootGridpane.add(pane, col, row);
+            }
+        }
     }
 }
+
+
+
+
